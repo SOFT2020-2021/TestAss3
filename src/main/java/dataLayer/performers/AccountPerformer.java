c60311947ebce6fa8cbcc64556de597726c32221
chrislind93 <clind_14@hotmail.com>
2020-04-03 20:38:51 +0200

    more spaghetti

--- src/main/java/dataLayer/performers/AccountPerformer.java
+++ src/main/java/dataLayer/performers/AccountPerformer.java
@@ -1,32 +1,62 @@
 package dataLayer.performers;
 
-
 import dataLayer.DAO;
 import dataLayer.entitites.Account;
 
-import java.sql.Statement;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class AccountPerformer {
 
-    public Account getOne() {
-        return null;
-    }
-
-    public boolean deleteOne() {
-        return false;
-    }
-
-    public boolean persist() {
+    public Account get(int id) {
+        Account acc = null;
         try {
-            Statement statement = new DAO().connection.createStatement();
-        } catch (SQLException e ){
-            System.out.println("Du er dårlig");
+            PreparedStatement ps = DAO.connection.prepareStatement("SELECT * FROM accounts WHERE id=?;");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                acc = new Account(null, null, rs.getInt(1));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        return false;
+        return acc;
     }
 
-    public boolean update() {
-        return false;
+    public void delete(int id) {
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("DELETE FROM accounts WHERE id=?;");
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void persist(int balance, int cpr, int cvr) {
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("INSERT INTO accounts(balance, customerCpr, bankCvr) VALUES(?,?,?);");
+            ps.setInt(1, balance);
+            ps.setInt(2, cpr);
+            ps.setInt(3, cvr);
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void update(int id, int balance, int cpr, int cvr) {
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("UPDATE accounts SET balance=?, cpr=?, cvr=? WHERE id=?;");
+            ps.setInt(1, balance);
+            ps.setInt(2, cpr);
+            ps.setInt(3, cvr);
+            ps.setInt(4, id);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/dataLayer/performers/BankPerformer.java
+++ src/main/java/dataLayer/performers/BankPerformer.java
@@ -1,23 +1,69 @@
 package dataLayer.performers;
 
 import businessLayer.Bank;
+import dataLayer.DAO;
+import dataLayer.entitites.Account;
+import dataLayer.entitites.MyBank;
+import dataLayer.entitites.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BankPerformer {
 
-    public Bank getOne() {
-        return null;
+    public Bank get(int cvr) {
+        Bank bank = null;
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("SELECT * FROM banks WHERE cvr = ?;");
+            ps.setInt(1, cvr);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                bank = new MyBank(Integer.parseInt(rs.getString(1)), rs.getString(2), new HashMap<Integer, Account>());
+            }
+            rs.close();
+            ps.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return bank;
     }
 
-    public boolean deleteOne() {
-        return false;
+    public void delete(int cvr) {
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("DELETE FROM banks WHERE cvr = ?;");
+            ps.setInt(1, cvr);
+            ps.executeUpdate();
+            ps.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    public boolean persist() {
-        return false;
+    public void persist(int cvr, String name) {
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("INSERT INTO banks(cvr,name) VALUES(?, ?);");
+            ps.setInt(1, cvr);
+            ps.setString(2, name);
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    public boolean update() {
-        return false;
+    public void update(int oldCvr, int cvr, String name) {
+        try {
+            PreparedStatement ps = DAO.connection.prepareStatement("UPDATE banks SET cvr=?, name=? WHERE cvr=?");
+            ps.setInt(1, cvr);
+            ps.setString(2, name);
+            ps.setInt(3, oldCvr);
+            ps.executeUpdate();
+            ps.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
